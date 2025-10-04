5253db2faa1c3447417be38550350094a681f6e3
Andres <marianoandres191200@hotmail.com>
2025-10-04 16:56:38 -0300

    filling some expense cards

--- src/components/expenses/header-grid/HeaderGrid.tsx
+++ src/components/expenses/header-grid/HeaderGrid.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 import ExpenseCard from "../expense-card/ExpenseCard";
 
 export default function HeaderGrid() {
@@ -8,19 +8,24 @@ export default function HeaderGrid() {
     <div className={styles.headerGridComponent}>
       <div className={styles.mainExpensesCost}>
         <ExpenseCard.Card>
-          <ExpenseCard.Label label="Total"/>
-          <ExpenseCard.Value value="$ 242.350,32"/>
+          <ExpenseCard.Label label="Total" />
+          <ExpenseCard.Value value="$ 242.350,32" />
         </ExpenseCard.Card>
       </div>
       <div className={styles.monthlyExpensesCost}>
-                <ExpenseCard.Card>
-          <ExpenseCard.Label label="Monthly"/>
-          <ExpenseCard.Value value="$ 75.114,55"/>
+        <ExpenseCard.Card>
+          <ExpenseCard.Label label="Monthly" />
+          <ExpenseCard.Value value="$ 75.114,55" />
         </ExpenseCard.Card>
       </div>
       <div className={styles.topExpensesCategories} />
       <div className={styles.timeLineExpensesTracker} />
-      <div className={styles.highestExpenseCost} />
+      <div className={styles.highestExpenseCost}>
+        <ExpenseCard.Card>
+          <ExpenseCard.Label label="Highest cost" />
+          <ExpenseCard.Value value="$ 29.000,00 (Pizza)" />
+        </ExpenseCard.Card>
+      </div>
     </div>
   );
 }
