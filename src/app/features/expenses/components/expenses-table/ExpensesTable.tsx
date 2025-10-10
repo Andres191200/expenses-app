364d81042643deaba49ea6f2b626d5e2dc9ffa3b
Andres <marianoandres191200@hotmail.com>
2025-10-10 01:59:52 -0300

    building types for table component

--- src/app/features/expenses/components/expenses-table/ExpensesTable.tsx
+++ src/app/features/expenses/components/expenses-table/ExpensesTable.tsx
@@ -1,40 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { use } from "react";
 import styles from "./styles.module.scss";
-import Image from "next/image";
-import Button, { EVariant } from "@/components/button/Button";
 import { ExpensesContext } from "../expenses-root/ExpensesRoot";
-import ExpenseCard from "../expense-card/ExpenseCard";
 
-export default function ExpensesTable() {
+type TColumn<T> = {
+  key: string,
+  label: string,
+  canSort: boolean,
+}
+
+type TExpensesTableProps<T> = {
+  columns: TColumn<T>[],
+  data: T,
+}
+
+export default function ExpensesTable<T extends Record<string, any>>({
+  columns,data 
+}: TExpensesTableProps<T>) {
   const { expenses } = use(ExpensesContext)!;
   return (
     <div className={styles.expensesTableComponent}>
-      {expenses.length === 0 ? (
-        <div className={styles.emptyExpenses}>
-          <div className={styles.emptyExpensesMessage}>
-            <Image
-              src={"/icons/info.svg"}
-              height={30}
-              width={30}
-              alt="info icon"
-            />
-            <p>There is no expenses yet</p>
-          </div>
-          <Button
-            label="Create one"
-            onClick={() => console.log("open expense creation modal")}
-            variant={EVariant.primary}
-          />
-        </div>
-      ) : (
-        <div className={styles.expensesMainGrid}>
-          {expenses.map((expense) => (
-            <ExpenseCard label={expense.title} key={expense.id} />
-          ))}
-        </div>
-
-      )}
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th key={column.key}>{column.label}</th>
+            ))}
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
+
+// <div className={styles.emptyExpenses}>
+//   <div className={styles.emptyExpensesMessage}>
+//     <Image
+//       src={"/icons/info.svg"}
+//       height={30}
+//       width={30}
+//       alt="info icon"
+//     />
+//     <p>There is no expenses yet</p>
+//   </div>
+//   <Button
+//     label="Create one"
+//     onClick={() => console.log("open expense creation modal")}
+//     variant={EVariant.primary}
+//   />
+// </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,47 @@
 import styles from "@/app/styles.module.scss";
-import ExpensesGrid from "@/app/features/expenses/components/expenses-grid/ExpensesGrid";
 import { prisma } from "@/lib/prisma";
 import { TExpense } from "./features/expenses/models/expense";
 import ExpensesHeaderGrid from "@/app/features/expenses/components/expenses-header-grid/ExpensesHeaderGrid";
 import ExpensesRoot from "./features/expenses/components/expenses-root/ExpensesRoot";
+import ExpensesTable from "@/app/features/expenses/components/expenses-table/ExpensesTable";
 
 export default async function Home() {
   const expenses:TExpense[] = await prisma.expense.findMany();
+
+  const columns = [
+    {
+    key:'name',
+    label:'Name',
+    canSort:true,
+  },
+  {
+    key:'category',
+    label:'Category',
+    canSort:true,
+  },
+  {
+    key:'value',
+    label:'Value',
+    canSort:true,
+  },
+  {
+    key:'date',
+    label:'Date',
+    canSort:true,
+  },
+  {
+    key:'actions',
+    label:'Actions',
+    canSort:false,
+  },
+  ]
+  
   return (
     <div className={styles.homePage}>
       <section className={styles.mainGridLayout}>
         <ExpensesRoot value={expenses}>
           <ExpensesHeaderGrid />
-          <ExpensesGrid />
+          <ExpensesTable columns={columns} data={expenses}/>
         </ExpensesRoot>
       </section>
     </div>
