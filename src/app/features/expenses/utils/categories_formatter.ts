f45c2cae417cb8a519928b1c9b48507db1999a32
Andres <marianoandres191200@hotmail.com>
2025-11-23 15:04:11 -0300

    formatCategoriesToLineChart function now groups categories correctly

--- src/app/features/expenses/utils/categories_formatter.ts
+++ src/app/features/expenses/utils/categories_formatter.ts
@@ -4,20 +4,27 @@ function formatCategoriesToLineChart(expenses:TExpense[]){
     const exp = expenses.reduce((acc:Partial<Record<string, number | string>>[], expense: TExpense) => {
         const categoryName = expense.category.name;
         const expenseValue = expense.value;
-        const existingCategoryIndex = acc.findIndex(item => Object.keys(item)[0] === categoryName);
+        const existingCategoryIndex = acc.findIndex(item => {
+            return item['name'] === categoryName;
+        });
+            console.log('existingCategoryIndex 1: ', existingCategoryIndex);
+            console.log('categoryName 1: ', categoryName);
+
 
         if (existingCategoryIndex !== -1) {
+            console.log('existingCategoryIndex 2: ', existingCategoryIndex);
+            console.log('categoryName 2: ', categoryName);
+
             const existingCategory = acc[existingCategoryIndex];
-            const currentValue = existingCategory[categoryName] || 0;
-            acc[existingCategoryIndex] = { [categoryName]: currentValue as number+ expense.value };
+            const currentValue = existingCategory[categoryName] || 0 as number;
+            acc[existingCategoryIndex] = { name: categoryName,value: Number(currentValue) + Number(expense.value) + Number(acc[existingCategoryIndex]['value'])};
+            console.log('transport: ', acc[existingCategoryIndex]);
         } else {
             acc.push({ name: categoryName, value: Number(expenseValue)});
         }
         return acc;
     
     }, []);
-
-    console.log('categories GROUPED: ', exp);
 
     return exp;
 }
