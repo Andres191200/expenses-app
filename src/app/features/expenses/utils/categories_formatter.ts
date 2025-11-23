054de3d8cb371aa9f574d506553d87e145f4c500
Andres <marianoandres191200@hotmail.com>
2025-11-23 15:11:47 -0300

    remove logs

--- src/app/features/expenses/utils/categories_formatter.ts
+++ src/app/features/expenses/utils/categories_formatter.ts
@@ -1,32 +1,33 @@
 import { TExpense } from "../models/expense";
 
-function formatCategoriesToLineChart(expenses:TExpense[]){
-    const exp = expenses.reduce((acc:Partial<Record<string, number | string>>[], expense: TExpense) => {
-        const categoryName = expense.category.name;
-        const expenseValue = expense.value;
-        const existingCategoryIndex = acc.findIndex(item => {
-            return item['name'] === categoryName;
-        });
-            console.log('existingCategoryIndex 1: ', existingCategoryIndex);
-            console.log('categoryName 1: ', categoryName);
+function formatCategoriesToLineChart(expenses: TExpense[]) {
+  const exp = expenses.reduce(
+    (acc: Partial<Record<string, number | string>>[], expense: TExpense) => {
+      const categoryName = expense.category.name;
+      const expenseValue = expense.value;
+      const existingCategoryIndex = acc.findIndex((item) => {
+        return item["name"] === categoryName;
+      });
 
+      if (existingCategoryIndex !== -1) {
+        const existingCategory = acc[existingCategoryIndex];
+        const currentValue = existingCategory[categoryName] || (0 as number);
+        acc[existingCategoryIndex] = {
+          name: categoryName,
+          value:
+            Number(currentValue) +
+            Number(expense.value) +
+            Number(acc[existingCategoryIndex]["value"]),
+        };
+      } else {
+        acc.push({ name: categoryName, value: Number(expenseValue) });
+      }
+      return acc;
+    },
+    []
+  );
 
-        if (existingCategoryIndex !== -1) {
-            console.log('existingCategoryIndex 2: ', existingCategoryIndex);
-            console.log('categoryName 2: ', categoryName);
-
-            const existingCategory = acc[existingCategoryIndex];
-            const currentValue = existingCategory[categoryName] || 0 as number;
-            acc[existingCategoryIndex] = { name: categoryName,value: Number(currentValue) + Number(expense.value) + Number(acc[existingCategoryIndex]['value'])};
-            console.log('transport: ', acc[existingCategoryIndex]);
-        } else {
-            acc.push({ name: categoryName, value: Number(expenseValue)});
-        }
-        return acc;
-    
-    }, []);
-
-    return exp;
+  return exp;
 }
 
-export {formatCategoriesToLineChart};
+export { formatCategoriesToLineChart };
