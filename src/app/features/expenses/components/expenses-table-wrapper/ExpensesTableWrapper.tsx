27cc5a4c752ad9d8d09c68b784dce3ad84d2b20f
Andres <marianoandres191200@hotmail.com>
2025-11-29 16:10:14 -0300

    validating errors

--- src/app/features/expenses/components/expenses-table-wrapper/ExpensesTableWrapper.tsx
+++ src/app/features/expenses/components/expenses-table-wrapper/ExpensesTableWrapper.tsx
@@ -7,7 +7,7 @@ import Button from "@/shared/components/button/Button";
 import { withToast } from "@/shared/components/hocs/withToast";
 import { expensesStore } from "@/shared/lib/store";
 import { createExpense } from "../../actions/createExpense";
-import { TCategory } from "@/shared/actions/getCategories";
+import { TCategory } from "../../models/category";
 
 type ExpensesTableWrapperProps = {
   expenses: TExpense[];
@@ -20,61 +20,108 @@ export default function ExpensesTableWrapper({
 }: ExpensesTableWrapperProps) {
   const [isAddingExpense, setIsAddingExpense] = useState(false);
   const [isLoading, setLoading] = useState(false);
-  const {addExpense} = expensesStore.getState();
-
-
-  const createExpenseWithToast = withToast(createExpense, {
-  success: "Expense created successfully",
-}, {
-  onSuccess: (expense) => {
-    addExpense(expense);
-  }
-});
-
-  async function _createExpense(expense: TExpense): Promise<void> {
-    setLoading(true);
-    await createExpenseWithToast({...expense, createdAt: new Date(Date.now())});
-    setIsAddingExpense(false);
-    setLoading(false);
-  }
-  
-
-  const columns = [
+  const { addExpense } = expensesStore.getState();
+  const [columns, setColumns] = useState([
     {
       key: "name",
       label: "Name",
       canSort: true,
+      error: {
+        status: false,
+        render: () => (
+          <p style={{ color: "red" }}>Expense name cannot be empty</p>
+        ),
+      },
       render: (expense: TExpense) => (
-        <p>{expense.id ? expense.title : "render input here"}</p>
+        <p className={styles.expenseName}>
+          {expense.id ? expense.title : "render input here"}
+        </p>
       ),
     },
     {
       key: "category",
       label: "Category",
       canSort: true,
+      error: {
+        status: false,
+        render: () => <React.Fragment />,
+      },
       render: (expense: TExpense) => <p>{expense.category?.name}</p>,
     },
     {
       key: "value",
       label: "Value",
       canSort: true,
+      error: {
+        status: false,
+        render: () => (
+          <p style={{ color: "red" }}>Value must be greater than 0</p>
+        ),
+      },
       render: (expense: TExpense) => <p>{expense.value}</p>,
     },
     {
       key: "date",
       label: "Date",
       canSort: true,
+      error: {
+        status: false,
+        render: () => <React.Fragment />,
+      },
       render: (expense: TExpense) => <p>{expense.createdAt?.toISOString()}</p>,
     },
     {
       key: "actions",
       label: "Actions",
       canSort: false,
+      error: {
+        status: false,
+        render: () => <React.Fragment />,
+      },
       render: () => (
         <button onClick={() => setIsAddingExpense(false)}>cancel</button>
       ),
     },
-  ];
+  ]);
+
+  const createExpenseWithToast = withToast(
+    createExpense,
+    {
+      success: "Expense created successfully",
+    },
+    {
+      onSuccess: (expense) => {
+        addExpense(expense);
+      },
+    }
+  );
+
+  async function _createExpense(expense: TExpense): Promise<void> {
+    setLoading(true);
+    await createExpenseWithToast({
+      ...expense,
+      createdAt: new Date(Date.now()),
+    });
+    setIsAddingExpense(false);
+    setLoading(false);
+  }
+
+  function setErrorsByColumnNumber(columnNumber:number){
+    setColumns(() => {
+      return columns.map((column, index) => {
+        if (index === columnNumber) {
+          return {
+            ...column,
+            error: {
+              status: true,
+              render: () => (column.error.render())
+            },
+          };
+        }
+        return column;
+      });
+    })
+  }
 
   return (
     <div className={styles.expensesTableWrapperComponent}>
@@ -95,6 +142,7 @@ export default function ExpensesTableWrapper({
         addEntry={() => {
           setIsAddingExpense(true);
         }}
+        setErrorsByColumnNumber={(columnNumber:number) => setErrorsByColumnNumber(columnNumber)}
         columns={columns}
         data={expenses}
         createEntry={(expense: TExpense) => _createExpense(expense)}
